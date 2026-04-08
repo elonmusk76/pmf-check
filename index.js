2504c6b3b8f1e02e8fb048a2a2d7886fae509ec2
elonmusk76 <careerquestmeta@gmail.com>
2026-04-08 23:32:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PMF Check</title>
+    <script type="module" src="/index.js"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
